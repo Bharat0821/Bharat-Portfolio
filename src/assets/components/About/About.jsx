43176d725edfc090af8bc9c9455e39efa9c04e84
Bharat <swamibharat2110@gmail.com>
2025-04-24 12:34:11 +0530

    Update About section

--- src/assets/components/About/About.jsx
+++ src/assets/components/About/About.jsx
@@ -13,21 +13,23 @@ const About = () => {
             
               <span className='w-95'>
                 <h1 className='text-xl md:text-2xl font-semibold leading-normal'>Frontend developer</h1>
-                <p className='text-sm md:text-md leading-tight'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur quo similique magni voluptatibus repudiandae itaque, cupiditate obcaecati dolor earum ipsum expedita labore perferendis!</p>
+                <p className='text-sm md:text-md leading-tight'>I craft clean, responsive, and user-friendly interfaces using HTML, CSS, JavaScript, and frameworks like Bootstrap and React. I focus on delivering smooth user experiences with a strong eye for design and attention to detail across all devices.</p>
               </span>
             </div>
             <div className='flex gap-3 py-4 '>
             
               <span className='w-95'>
                 <h1 className='text-xl md:text-2xl font-semibold leading-normal'>Backend developer</h1>
-                <p className='text-sm md:text-md leading-tight'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur quo similique magni voluptatibus repudiandae itaque, cupiditate obcaecati dolor earum ipsum expedita labore perferendis!</p>
+                <p className='text-sm md:text-md leading-tight'>I build secure and scalable server-side logic using technologies like PHP and Node.js. I work with APIs, manage databases, and ensure seamless communication between the frontend and backend components of web applications.</p>
               </span>
             </div>
             <div className='flex gap-3 py-4 '>
            
               <span className='w-95'>
-                <h1 className='text-xl md:text-2xl font-semibold leading-normal'>DataBase developer</h1>
-                <p className='text-sm md:text-md leading-tight'> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur quo similique magni voluptatibus repudiandae itaque, cupiditate obcaecati dolor earum ipsum expedita labore perferendis!</p>
+                <h1 className='text-xl md:text-2xl font-semibold leading-normal'>FullStack developer</h1>
+                <p className='text-sm md:text-md leading-tight'>With expertise in both frontend and backend development, I develop complete web applications—from design to deployment. I’m comfortable handling everything from UI creation to database management, using tools like React, PHP, and MySQL to bring ideas to life efficiently.
+
+</p>
               </span>
             </div>
           </ul>
