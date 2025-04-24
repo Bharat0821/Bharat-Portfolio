992a42b2a4a84077c01cbe44f24ec121d3c9067a
Bharat <swamibharat2110@gmail.com>
2025-04-24 11:48:51 +0530

    Update Home.jsx

--- src/assets/components/Home/Home.jsx
+++ src/assets/components/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <div className='md:w-2/4 md:pt-10 '>
             <h1 className='text-3xl md:text-6xl font-bold flex leading-normal tracking-tighter'><TextChanger/></h1>
             <p className='text-xl md:md-2xl tracking-tight  '>I am a passionate web developer dedicated to building dynamic, user-friendly, and responsive websites. With expertise in frontend and backend development, I craft seamless digital experiences that blend creativity with functionality. Let's create something amazing together!</p>
-           <a href="mailto:swamibharat2110@gmail.com"><button className='mt-5 md:md-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'> Contact Me</button></a> 
+           <button className='mt-5 md:md-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'><a href="mailto:swamibharat2110@gmail.com"></a> Contact Me</button>
         </div>
         <div className='transition-all duration-1000'>
             <img className='max-w-md w-100 mr-10 hover:scale-110' src={avatarImg} alt="" />
